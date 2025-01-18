4c27277b938757d25b5c065de19d9a0bc8df84c9
Paolo <30302837+PaoloKa@users.noreply.github.com>
2025-01-18 11:19:41 +0100

    basic homepage

--- dnd-mapper/src/app/page.tsx
+++ dnd-mapper/src/app/page.tsx
@@ -1,95 +1,81 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <Box
+      className={styles.page}
+      sx={{
+        backgroundImage: `url("https://static1.srcdn.com/wordpress/wp-content/uploads/2024/08/d-d-farmer-background.jpg")`,
+      }}
+    >
+      <Box className={styles.main}>
+        <Typography
+          variant="h2"
+          sx={{
+            fontFamily: "'Uncial Antiqua', serif", // Fantasy-style font
+            color: "#D52A2A", // Optional: A bold, D&D-themed red
+            textShadow: "2px 2px 4px #000000", // Optional: Adds a dramatic shadow
+          }}
+        >
+          D&amp;D Mapper
+        </Typography>
         <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Get started by editing your maps.</li>
+          <li>Pick an option below</li>
         </ol>
 
         <div className={styles.ctas}>
-          <a
+          <Link
+            href={"/map"}
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            Create new map
+          </Link>
+          <Link
+            href="/info"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.secondary}
           >
             Read our docs
-          </a>
+          </Link>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          padding: "1rem",
+          color: "white",
+        }}
+      >
+        <Box
+          component={"a"}
+          href="https://github.com/PaoloKa/dnd-mapper"
           target="_blank"
           rel="noopener noreferrer"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            color: "black",
+          }}
         >
-          <Image
+          <img
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
+            src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000"
             alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Github
+        </Box>
+      </Box>
+    </Box>
   );
 }
